165642f8a6caf395d07f9a0a0b7dff19ffcc5c55
Boyapati Shree Harsha <boyapatishreeharsha@gmail.com>
2024-01-15 16:45:59 +0530

    week 6 done

---
+++ week-6/2-use-callback/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import { Assignment1 } from './components/Assignment1'
+import { Assignment2 } from './components/Assignment2'
+// import CallbackExample  from './components/CallbackExample'
+
+function App() {
+  return (
+    <>
+      {/* <Assignment1 /> */}
+      <Assignment2 />
+      {/* <CallbackExample/> */}
+    </>
+  )
+}
+
+export default App
---
+++ week-6/2-use-callback/src/components/CallbackExample.jsx
@@ -0,0 +1,28 @@
+import React, { useState, useCallback } from 'react';
+
+const ChildComponent = ({ onClick }) => {
+  console.log('ChildComponent rendering...');
+  return <button onClick={onClick}>Click me</button>;
+};
+
+const CallbackExample = () => {
+  const [count, setCount] = useState(0);
+
+  // Regular callback function
+  const handleClick = () => {
+    console.log('Button clicked!');
+    setCount((prevCount) => prevCount + 1);
+  };
+
+  // Memoized callback using useCallback
+  const memoizedHandleClick = useCallback(handleClick, []);
+
+  return (
+    <div>
+      <p>Count: {count}</p>
+      <ChildComponent onClick={memoizedHandleClick} />
+    </div>
+  );
+};
+
+export default CallbackExample;
